124890684b7752ddb18d0d628cd7dbd8f0399663
Fafruch <fafruszek@gmail.com>
2017-11-13 16:07:47 +0100

    Add bootstrap support,add basic classes and use :authenticate_user!

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
   end
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+  devise_scope :user do
+    get 'sign_up', to: 'devise/registrations#new'
+  end
 
   root to: 'home#index'
 end
