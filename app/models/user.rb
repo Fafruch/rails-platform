f91a375bfbd8904e80dc7c43ce85a52e0db71d6e
Fafruch <fafruszek@gmail.com>
2017-11-17 22:55:21 +0100

    Show errors when editing a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
             uniqueness: true,
             presence: true,
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
-                      message: 'has bad format' }
+                      message: 'has a bad format' }
   validates :phone_number,
             presence: true,
             format: { with: /\A(\+48)?\s?\d{3}\s?\d{3}\s?\d{3}\z/,
-                      message: 'has bad format' }
+                      message: 'has a bad format' }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
